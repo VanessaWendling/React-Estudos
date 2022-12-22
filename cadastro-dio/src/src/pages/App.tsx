4f074589a9447de4e26b151be80654961d17c972
VanessaWendling <vanessamw2001@hotmail.com>
2022-12-21 23:28:32 -0300

    projeto clone tela cadastro

---
+++ cadastro-dio/src/src/pages/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h2>
+        A plataforma para você aprender com experts, dominar as principais
+        tecnologias e entrar mais rápido nas empresas mais desejadas.
+      </h2>
+      <h2>Comece agora grátis</h2>
+      <p>Crie sua conta e make the change._</p>
+      <p>
+        Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas
+        de Privacidade e os Termos de Uso da DIO.
+      </p>
+      <h6>Já tenho conta. Fazer login</h6>
+    </div>
+  );
+}
+
+export default App;
